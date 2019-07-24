4a3c77411580084b44445be8652a9bdcaa29c644
Ben Gorgan <benrgorgan@gmail.com>
2019-07-23 19:26:13 -0500

    added drawing ability

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ let gridSize = 16;
 let divWidth = 100/(gridSize);
 let mainScreen = document.querySelector('#screen');
 
-for (var i = 1; i <= gridSize; i++) {
-  for (var j = 1; j <= gridSize; j++) {
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'pixel');
-    newDiv.style.width = divWidth.toString()+'%';
-    newDiv.style.height = divWidth.toString()+'%';
-    mainScreen.appendChild(newDiv);
+gridStart();
+let pixels = document.querySelectorAll('.pixel');
+for (var i = 0; i < pixels.length; i++) {
+  pixels[i].addEventListener('mouseover', etch);
+}
+
+
+function gridStart() {
+  for (var i = 1; i <= gridSize; i++) {
+    for (var j = 1; j <= gridSize; j++) {
+      let newDiv = document.createElement('div');
+      newDiv.setAttribute('class', 'pixel');
+      newDiv.style.width = divWidth.toString()+'%';
+      newDiv.style.height = divWidth.toString()+'%';
+      mainScreen.appendChild(newDiv);
+    }
   }
+}
+
+function etch(e) {
+  console.log(e.target);
+  e.target.style.backgroundColor = 'black';
 }
